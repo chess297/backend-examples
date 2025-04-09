ee99ce1d9a77579919b2bef71440d97c248576b9
chess <viky.chen@outlook.com>
2025-04-09 19:09:58 +0800

    feat: nestjs-example add swagger

--- nodejs/nestjs-example/src/main.ts
+++ nodejs/nestjs-example/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api/v1');
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Example')
+    .setDescription('The NestJS Example API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
