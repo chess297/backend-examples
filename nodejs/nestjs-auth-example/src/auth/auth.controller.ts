6e021752c715a6f8ce7e460b8db075f035ea2598
chess <viky.chen@outlook.com>
2025-04-14 20:20:25 +0800

    feat(nodejs): add SigninRequest

--- nodejs/nestjs-auth-example/src/auth/auth.controller.ts
+++ nodejs/nestjs-auth-example/src/auth/auth.controller.ts
@@ -11,6 +11,7 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { ApiBody, ApiProperty } from '@nestjs/swagger';
 import { Request, Response } from 'express';
 
 declare module 'express-session' {
@@ -21,15 +22,27 @@ declare module 'express-session' {
   }
 }
 
+class SigninRequest {
+  @ApiProperty({ example: 'example@example.com' })
+  email: string;
+  @ApiProperty({
+    example: '123456',
+  })
+  password: string;
+}
+
 @Controller('auth')
 export class AuthController {
   private readonly logger = new Logger(AuthController.name);
   constructor(private readonly jwtService: JwtService) {}
+
+  @ApiBody({
+    type: SigninRequest,
+  })
   @Post('signin')
   signin(@Req() req: Request, @Res() res: Response, @Body() body: User) {
     req.session.isLogin = true;
     req.session.email = body.email;
-
     res.json({
       access_token: '',
     });
@@ -42,17 +55,14 @@ export class AuthController {
 
   @UseGuards(SessionGuard)
   @Post('signout')
-  async signout(@Req() req: Request, @Res() res: Response) {
-    res.clearCookie(SESSION_ID_COOKIE_KEY);
-    // req.session.
-    await new Promise((resolve) => {
-      req.session.destroy((err) => {
-        if (err) {
-          this.logger.error(err);
-        }
-        resolve(true);
-      });
+  signout(@Req() req: Request, @Res() res: Response) {
+    req.session.destroy((err) => {
+      if (err) {
+        this.logger.error(err);
+      }
     });
+    res.clearCookie(SESSION_ID_COOKIE_KEY);
+
     res.json({
       success: true,
     });
